b7729cb928524a67d9eae95c9e02a0356fa2fc57
Andrew Gerst <gerst20051@gmail.com>
2012-06-02 13:45:40 -0400

    improved code to update game data

--- src/load.php
+++ src/load.php
@@ -4,13 +4,13 @@ chdir("/xampp/HomenetSpaces/hnsdesktop/");
 
 if (isset($_GET['id'])) {
 $id = trim($_GET['id']);
-$username = $_SESSION['username'];
+if (isset($_SESSION['username'])) $username = $_SESSION['username']; else $username = "GUSER";
 $userid = $_SESSION['user_id'];
 
 if (isset($_GET['action'])) $action = trim($_GET['action']);
 if (isset($_GET['data'])) $data = trim($_GET['data']);
-$newdata = "$username|$data";
-$newplayerslist = "";
+$newdata = "$username,$data";
+$newplayerslist = '';
 
 switch ($id) {
 case 'game':
@@ -19,36 +19,53 @@ switch ($action) {
 case 'update':
 
 $file = "game/playerlist.txt";
-
-if ($fhandle = fopen($file, 'r')) {
-	if (flock($fhandle, LOCK_EX)) {
-		$playerslist = trim(fread($fhandle, filesize($file)));
-		fclose($fhandle);
+$playerslist = file_get_contents($file);
+if ($playerslist != '') {
+	if (strpos($playerslist, "|") === true) {
+		$playerlist = explode("|", $playerslist); $exists = false; $playerinfo = "";
+		foreach ($playerlist as $players) {
+			$player = explode(",", $players);
+			if ($username == $player[0]) { $exists = true; $playerinfo = $players; }
+		}
+		if ($exists === true) {
+			// while (strpos($playerslist, $playerinfo) !== false)
+			// $newplayerslist = str_replace($playerinfo, $newdata, $playerslist);
+			// $newplayerslist = $playerslist;
+		} // else $newplayerslist = $playerslist . "|" . $newdata;
+	} else {
+		$player = explode(",", $playerslist);
+		if ($username == $player[0]) $newplayerslist = $newdata;
+		else $newplayerslist = $playerslist . "|" . $newdata;
 	}
+} else $newplayerslist = $newdata;
+echo $newplayerslist;
+file_put_contents($file, $newplayerslist, LOCK_EX);
+
+/*
+if ($fhandle = fopen($file, 'r')) {
+	if (flock($fhandle, LOCK_EX)) $playerslist = trim(fread($fhandle, filesize($file)));
+	fclose($fhandle);
 }
 
 if ($fhandle = fopen($file, 'w')) {
 	if (flock($fhandle, LOCK_EX)) {
-		if ($playerslist != "") {
-			if (strpos($playerslist, ",") === true) {
-				$playerlist = explode(",", $playerslist); $exists = false; $playerinfo = "";
+		if ($playerslist != '') {
+			if (strpos($playerslist, "|") === true) {
+				$playerlist = explode("|", $playerslist); $exists = false; $playerinfo = "";
 				foreach ($playerlist as $players) {
-					$player = explode("|", $players);
-					if (in_array($username, $player)) {
-						$exists = true; $playerinfo = $players;
-					}
+					$player = explode(",", $players);
+					if ($username == $player[0]) { $exists = true; $playerinfo = $players; }
 				}
 				if ($exists === true) {
-					while (strpos($newplayerslist, $playerinfo) !== false) {$playerslist = str_replace($playerinfo, $newdata, $playerslist);}
-					$newplayerslist = $playerslist;
-				} else $newplayerslist = $playerslist . "," . $newdata;
+					// while (strpos($playerslist, $playerinfo) !== false)
+					$newplayerslist = str_replace($playerinfo, $newdata, $playerslist);
+					// $newplayerslist = $playerslist;
+				} else $newplayerslist = $playerslist . "|" . $newdata;
 			} else {
-				$player = explode("|", $playerslist);
-				if (in_array($username, $player)) $newplayerslist = $newdata;
-				else $newplayerslist = $playerslist . "," . $newdata;
+				$player = explode(",", $playerslist);
+				if ($username == $player[0]) $newplayerslist = $newdata;
+				else $newplayerslist = $playerslist . "|" . $newdata;
 			}
-		} else {
-			$newplayerslist = $newdata;
 		}
 		if (is_writeable($file)) fwrite($fhandle, $newplayerslist);
 		echo $newplayerslist;
@@ -57,6 +74,7 @@ if ($fhandle = fopen($file, 'w')) {
 } else {
 	echo "x";
 }
+*/
 
 break;
 }
